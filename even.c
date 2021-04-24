68e985ad6dfc22b2acc2ef7690618cdcc8b8e8ce
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-24 15:46:37 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int a;
+  printf("Enter the number :");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+    printf("The Number is Even\n");
+  }
+  else
+  {
+    printf("Invalid Number\n");
+  }
+}
