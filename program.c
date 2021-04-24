8558d09520cc5e181fcf2ccd5541bdcfb463ffe7
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-24 14:55:33 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,24 @@
+
+/*
+program to find the multiples of eleven
+Their sum and 
+Count
+*/
+#include<stdio.h>
+void main()
+{
+  int n=100, s=0,c=0;
+  while(n<=500)
+  {
+    if(n%11==0)
+    {
+      printf("%d ",n);
+      s=s+n;
+      c=c+1;
+    }
+    n=n+1;
+  }
+  printf("\n");
+  printf("the sum is=%d\n",s);
+  printf("count=%d\n",c);
+}
