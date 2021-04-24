e710e8bf57fc679a29c9f20b22bb7647ddf9e77d
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-24 13:31:50 +0530

    Add files via upload

---
+++ sumdig.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int n,s=0,d;
+ printf("Enter the number:");
+ scanf("%d",&n);
+ printf("sum of the digits of %d is",n);
+ while(n>0)
+ {
+   d=n%10;
+   s=s+d;
+   n=n/10;
+ }
+ printf("sum of the digits of %d is",s);
+}
