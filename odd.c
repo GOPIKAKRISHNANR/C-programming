a05e914f66323c1aad22d1929a203feaf3279851
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-24 15:46:11 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,16 @@
+//program to check the number is odd or not
+#include<stdio.h>
+void main()
+{
+  int a;
+  printf("Enter the number :");
+  scanf("%d",&a);
+  if(a%2!=0)
+  {
+    printf("The Number is Odd\n");
+  }
+  else
+  {
+    printf("Invalid Number\n");
+  }
+}
