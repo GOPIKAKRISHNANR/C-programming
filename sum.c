12d8d2643350ace5723e5c5341d903915d8739e4
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-24 13:30:54 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,s;
+  printf("Enter the two numbers:");
+  scanf("%d%d",&a,&b);
+  printf("\n*************\n" );
+  s=a+b;
+  printf("%d+%d=%d",a,b,s);
+  printf("\n");
+
+}
