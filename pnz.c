1fdfa2031521f5da572f52857e3ef8ec0066636f
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-24 13:40:53 +0530

    Add files via upload

---
+++ pnz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("%d is a positive number");
+    }
+    else if("n<0")
+    {
+        printf("%d is a negative number");
+    }
+    else
+    {
+        printf("the number is zero");
+    }
+    printf("\n");
+}
