c2faa190ecbead199d9284586644f1e0c89aea3c
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-26 10:31:17 +0530

    Add files via upload

---
+++ naturalsum.c
@@ -0,0 +1,13 @@
+//program to check the sum of first n natural numbers
+#include<stdio.h>
+int main()
+{
+  int n,i=1,sum=0;
+  printf("Enter how many natural numbers :");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    sum=sum+i;
+  }
+    printf("The sum of %d natural numbers is %d\n",n,sum);
+}
